79a66f780991469e517b4a1665ba9aae071a5c8a
Zafar <mdabuzafar78@gmail.com>
2017-11-15 14:46:58 +0530

    Restructure the file

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ function sessionMinus() {
 
 
 // event listeners and function for break length
-document.getElementById('break-plus').addEventListener('click', breakPlus);
-document.getElementById('break-minus').addEventListener('click', breakMinus);
-let breakTime = document.getElementById('break-time').innerText;
-
+    
 function breakPlus() {
   breakTime = Number(breakTime);
   document.getElementById('break-time').innerText = breakTime < 15 ? breakTime += 1 : breakTime;
