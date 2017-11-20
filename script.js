71d07855b17f2a553cb82ec4d0ab5a26617dfc1b
Zafar <mdabuzafar78@gmail.com>
2017-11-20 20:12:27 +0530

    Make the startTimer function work

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function sessionPlus() {
 
 function sessionMinus() {
   sessionTime = Number(sessionTime);
-  sessionTime = sessionTime > 5 ? sessionTime -= 5 : sessionTime;
+  sessionTime = sessionTime > 1 ? sessionTime -= 1 : sessionTime;
   document.getElementById('length-of-session-time').innerText = sessionTime;
   document.getElementById('length-of-timer').innerText = sessionTime;
 }
@@ -31,15 +31,38 @@ function breakPlus() {
 
 function breakMinus() {
   breakTime = Number(breakTime);
-  document.getElementById('length-of-break-time').innerText = breakTime > 2 ? breakTime -= 1 : breakTime;
+  document.getElementById('length-of-break-time').innerText = breakTime > 1 ? breakTime -= 1 : breakTime;
 }
 
-function startTimer(timeInMilli, type) {
+function startTimer(length, type) {
   document.getElementById('session-plus').disabled = true;
   document.getElementById('session-minus').disabled = true;
   document.getElementById('break-plus').disabled = true;
   document.getElementById('break-minus').disabled = true;
   document.getElementById('start').disabled = true;
+
+  // current time plus the length of session in milli seconds
+  let finalTime = new Date().getTime() + length * 60000;
+
+  let runTimer = setInterval(function() {
+    let currentTime = new Date().getTime();
+    let distance = finalTime - currentTime;
+
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.querySelector('#timer').innerHTML = `${minutes} m ${seconds} s`;
+
+    // If session is over start break
+    if (distance <= 1000) {
+      clearInterval(runTimer);
+      if (type == 'session') {
+        startTimer(breakTime, 'break');
+      } else {
+        startTimer(sessionTime, 'session');
+      }
+    }
+  }, 1000);
 }
 
 function resetTimer() {
